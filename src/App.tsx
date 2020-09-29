7c7737236335d32042d75df27c8a7f3ef92fef2f
zachary-nguyen <43250829+zachary-nguyen@users.noreply.github.com>
2020-09-29 13:33:06 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Routes from "./routes/Routes";
+
+function App() {
+  return (
+    <div className="App">
+      {/*<Routes/>*/}
+      <h1> ICUCODE </h1>
+    </div>
+  );
+}
+
+export default App;
